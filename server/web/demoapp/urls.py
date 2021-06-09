8644625a00b339a924b0ef305685423d6938695b
Siyu Qian <david.s.qian@gmail.com>
2021-06-09 21:17:06 +1200

    Implement add event label feature

--- server/web/demoapp/urls.py
+++ server/web/demoapp/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('api/v1/stress', views.stress_index, name="stress_index"),
     path('api/v1/uuid', views.uuid_index, name="uuid_index"),
     path('report', views.report_index, name="report_index"),
+    path('report/label/add', views.report_add_label_index, name="report_add_label_index"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
